39fd477aad2a74a22231e632bf476ffac33610e6
Zoltan MG <zoltanmoragarcia@gmail.com>
2021-09-13 20:19:30 -0500

    descuentos

--- descuentos.js
+++ descuentos.js
@@ -5,14 +5,41 @@ function calcularPrecioConDescuento(precio, descuento) {
 }
 
 function onClickButtonPriceDiscount() {
-  const inputPrice = document.getElementById("InputPrice")
-  const priceValue = inputPrice.value
-  const inputDiscount = document.getElementById("InputDiscount")
-  const dicountValue = inputDiscount.value
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("InputCoupon");
+  const couponValue = inputDiscount.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, dicountValue)
-  const resultPrice = document.getElementById("ResultPrice")
-  resultPrice.innerText = "El precio con descuento es $" + precioConDescuento
+  const coupons = [
+    {
+      name: "JuanDC_es_Batman",
+      discount: 15,
+    },
+    {
+      name: "pero_no_le_digas_a_nadie",
+      discount: 30,
+    },
+    {
+      name: "es_un_secreto",
+      discount: 25,
+    },
+  ];
+
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+  };
+
+  const userCoupon = coupons.find(isCouponValueValid);
+
+  if (!userCoupon) {
+    alert("El cupón " + couponValue + " no es válido");
+  } else {
+    const descuento = userCoupon.discount;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultPrice");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
 }
 
 // console.log({
